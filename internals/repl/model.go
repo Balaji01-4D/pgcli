57385f8f06a15233d1ff3428fbfecda4cda19b47
Balaji01-4D <j.balaji2468@gmail.com>
2025-12-28 00:36:22 +0530

    feat: add print, printerr, printTime

--- internals/repl/model.go
+++ internals/repl/model.go
@@ -1,28 +1,46 @@
 package repl
 
 import (
-	"github.com/elk-language/go-prompt"
-)
+	"io"
+	"os"
+	"time"
 
+	"github.com/elk-language/go-prompt"
+	"github.com/fatih/color"
+)
 
 type Repl struct {
 	db string
-}
-
-func (r *Repl) GetPrefix() string {
-	return r.db + "> "
-}
-
-func (r *Repl) GetLine() string {
-	text := prompt.Input(
-		prompt.WithPrefix(r.GetPrefix()),
-	)
-	return text
 }
 
 func NewModel(db string) *Repl {
 	return &Repl{db: db}
 }
 
+func (r *Repl) GetPrefix() string {
+	return r.db + "> "
+}
 
+func (r *Repl) Read() string {
+	text := prompt.Input(
+		prompt.WithPrefix(r.GetPrefix()),
+	)
+	return text
+}
 
+func (r *Repl) PrintError(err error) {
+	c := color.New(color.FgRed)
+	c.Fprintf(os.Stderr, "%v\n", err)
+}
+
+func (r *Repl) PrintTime(time time.Duration) {
+	c := color.New(color.FgCyan)
+	c.Fprintf(os.Stderr, "Time: %.3fs\n", time.Seconds())
+}
+
+func (r *Repl) Print(output string) {
+	EchoViaPager(func(w io.Writer) error {
+		io.WriteString(w, output)
+		return nil
+	})
+}
